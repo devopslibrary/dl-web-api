4851f0e916e0eee3abf6bca45eb9a3e5fb385ca4
Ken Erwin <ken@devopslibrary.com>
2019-11-12 19:23:48 -0500

    is it redis?

--- library/redis/redisDB.test.js
+++ library/redis/redisDB.test.js
@@ -1,7 +1,12 @@
 const redisDB = require("./redisDB");
 
 test("Should be able to talk to Redis properly", async () => {
-  await redisDB.set('testkey', 'testvalue')
-  const output = await redisDB.get('testkey');
+  await redisDB.set("testkey", "testvalue");
+  const output = await redisDB.get("testkey");
   expect(output).toEqual("testvalue");
+});
+
+afterAll(async done => {
+  redisDB.unref();
+  done();
 });
