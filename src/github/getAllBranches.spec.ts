0b1e7c3178fbcfbc2314db5e1ced4d3f9922613b
Ken Erwin <ken@devopslibrary.com>
2019-11-23 00:24:30 -0500

    fixed test for getAllBranches

--- src/github/getAllBranches.spec.ts
+++ src/github/getAllBranches.spec.ts
@@ -2,7 +2,7 @@ import { getAllBranches } from './getAllBranches';
 import { BranchModel } from './models/branch';
 
 test('Given an org, should return array of branches', async () => {
-  const branches: BranchModel[] = await getAllBranches(11233903);
+  const branches: BranchModel[] = await getAllBranches('devopslibrary');
   expect(branches[0].id).toBeDefined();
   expect(branches[0].isProtected).toBeDefined();
   expect(branches[0].lastCommitDate).toBeDefined();
