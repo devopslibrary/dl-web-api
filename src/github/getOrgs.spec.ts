d3d883793506b31f9955026a8d0f319eb1055994
Ken Erwin <ken@devopslibrary.com>
2019-11-25 00:25:27 -0500

    All tests running now in ado

--- src/github/getOrgs.spec.ts
+++ src/github/getOrgs.spec.ts
@@ -3,11 +3,26 @@ import { getManagementApiToken } from '../auth0/getManagementApiToken';
 import { UserModel } from './models/user';
 import { OrgModel } from './models/org';
 import { getOrgs } from './getOrgs';
+// import { apiQuery } from '../infra/apiHelper/index';
+
+// Mock the Query
+const apiQuery = async function(filename, args) {
+  const fakeData = `{
+    "orgById": {
+      "createdAt": "2019-10-27T00:28:55+00:00",
+      "id": 11233903,
+      "installationId": 4631459,
+      "name": "devopslibrary",
+      "nodeId": "WyJPcmdzIiwxMTIzMzkwM10="
+    }
+  }`;
+  return JSON.parse(fakeData);
+};
 
 test('Given a real ID, should return a proper Github User', async () => {
   const authToken = await getManagementApiToken();
   const user: UserModel = await getUser(authToken, '5382669');
-  const orgs: OrgModel[] = await getOrgs(user.token);
+  const orgs: OrgModel[] = await getOrgs(apiQuery, user.token);
   expect(orgs[0].avatar).toBeDefined();
   expect(orgs[0].id).toBeDefined();
   expect(orgs[0].installationId).toBeDefined();
--- src/github/getOrgs.ts
+++ src/github/getOrgs.ts
@@ -1,13 +1,10 @@
 import request = require('request-promise');
-import * as graphqlRequest from 'graphql-request';
-import { readFileSync } from 'fs';
 import dotenv = require('dotenv');
 import { OrgModel } from './models/org';
-import e = require('express');
 dotenv.config();
 
 // Returns all orgs that a user belongs to (provided they allow us to get that info!)
-export async function getOrgs(githubToken): Promise<OrgModel[]> {
+export async function getOrgs(apiQuery, githubToken): Promise<OrgModel[]> {
   // Retrieve Orgs from Github
   const options = {
     method: 'GET',
@@ -29,14 +26,9 @@ export async function getOrgs(githubToken): Promise<OrgModel[]> {
   return await Promise.all(
     orgs.map(async org => {
       // Get Orgs from Database
-      const query = readFileSync(__dirname + '/getOrg.graphql', 'utf8');
-      const data = await graphqlRequest.request(
-        process.env.DATABASE_API,
-        query,
-        {
-          id: org.id,
-        },
-      );
+      const data: any = await apiQuery(__dirname + '/getOrg.graphql', {
+        id: org.id,
+      });
       if (data.orgById) {
         org.installed = true;
         org.installationId = data.orgById;
